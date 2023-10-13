83e6c3e29c64fd43d2861b0ef95a39e3e309dc5f
lahariyandapalli <ylahari6@gmail.com>
2023-10-13 10:20:26 +0530

    updated

--- src/Components/LoginManager/LoginManager.js
+++ src/Components/LoginManager/LoginManager.js
@@ -1,25 +1,125 @@
 import React, { useState } from 'react';
-import Login from '../Login/Login'; // Import Login component
-import Password from '../Password/Password'; // Import Password component
-import Otp from '../Otp/Otp'; // Import Otp component
+import './LoginManager.css';
+import { useNavigate } from 'react-router-dom';  // Import useNavigate
+
+
 
 function LoginManager() {
-  const [currentPage, setCurrentPage] = useState('login');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [otp, setOtp] = useState('');
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
+  const [currentPage, setCurrentPage] = useState('login');
+  const navigate = useNavigate();  // Define navigate using useNavigate
+
+  const handleProceed = (direction) => {
+    if (direction === 'next') {
+      if (currentPage === 'login') {
+        setCurrentPage('password');
+      } else if (currentPage === 'password') {
+        setCurrentPage('otp');
+      } else {
+        // Handle OTP submission logic
+      }
+    } else if (direction === 'back') {
+      if (currentPage === 'password') {
+        setCurrentPage('login');
+      } else if (currentPage === 'otp') {
+        setCurrentPage('password');
+      }
+    }
+  };
+
+  const handleNavigateToHome = () => {
+    // Use the navigate function to go to the "Home" route
+    navigate('/');
   };
 
   return (
-    <div>
-    
+    <div className="card-container-container">
+      <img src="/Images/E-Wallet-pana.png" alt="E-Wallet Image" className="small-image" />
       {currentPage === 'login' && (
-        <Login onNext={() => handlePageChange('password')} />
+        <div className="card-container login-box">
+          <h2>Login</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                placeholder="Enter your email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="button-container">
+              <button type="button" onClick={() => handleProceed('next')} className="login-button">
+                Next
+              </button>
+            </div>
+          </form>
+        </div>
       )}
+
       {currentPage === 'password' && (
-        <Password onNext={() => handlePageChange('otp')} />
+        <div className="card-container password-form">
+          <h2>Password</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="password">Enter your password:</label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <div className="button-container">
+              <button type="button" onClick={() => handleProceed('back')} className="login-button">
+                Back
+              </button>
+              <button type="button" onClick={() => handleProceed('next')} className="login-button">
+                Next
+              </button>
+            </div>
+          </form>
+        </div>
       )}
-      {currentPage === 'otp' && <Otp />}
+
+      {currentPage === 'otp' && (
+        <div className="card-container otp-box">
+          <h2>Enter OTP</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="otp">OTP</label>
+              <input
+                type="text"
+                id="otp"
+                className="form-control"
+                placeholder="Enter OTP"
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+              />
+            </div>
+            <p className="resend-link">
+              Didn't receive OTP? <span>Resend OTP</span>
+            </p>
+            <div className="button-container">
+              <button type="button" onClick={() => handleProceed('back')} className="login-button">
+                Back
+              </button>
+              <button
+                type="button"
+                onClick={handleNavigateToHome}
+                className="login-button"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- src/Components/Otp/Otp.js
+++ src/Components/Otp/Otp.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import './Otp.css';
+// import React from 'react';
+// import './Otp.css';
 
-function Otp() {
-  return (
-    <div className="container">
-      <div className="otp-box">
-        <h2>Enter OTP</h2>
-        <form>
-          <div className="form-group">
-            <label htmlFor="otp">OTP</label>
-            <input type="text" id="otp" className="form-control" placeholder="Enter OTP" />
-          </div>
-          <div className="btn-container">
-            <button type="button" className="submit-button">
-              Submit
-            </button>
-          </div>
-        </form>
-        <p className="resend-link">
-          Didn't receive OTP? <span>Resend OTP</span>
-        </p>
-      </div>
-    </div>
-  );
-}
+// function Otp() {
+//   return (
+//     <div className="container">
+//       <div className="otp-box">
+//         <h2>Enter OTP</h2>
+//         <form>
+//           <div className="form-group">
+//             <label htmlFor="otp">OTP</label>
+//             <input type="text" id="otp" className="form-control" placeholder="Enter OTP" />
+//           </div>
+//           <div className="btn-container">
+//             <button type="button" className="submit-button">
+//               Submit
+//             </button>
+//           </div>
+//         </form>
+//         <p className="resend-link">
+//           Didn't receive OTP? <span>Resend OTP</span>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Otp;
+// export default Otp;
