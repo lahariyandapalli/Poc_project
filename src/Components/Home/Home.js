63ead5c1bced6887cb090cf5cf43653667d665da
lahariyandapalli <ylahari6@gmail.com>
2023-10-25 19:17:25 +0530

    new updates

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import './Home.css'
 import Header from '../Header/Header'
@@ -7,42 +7,22 @@ import { useNavigate } from 'react-router-dom';
 const Home = () => {
   const navigate = useNavigate(); // Create a navigate function
  
-=======
-import React from 'react';
-import './Home.css';
-import Header from '../Header/Header';
-import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
-  const navigate = useNavigate();
-
->>>>>>> 95ad50e445b23d590a7deb6951c25e40c200a56a
   const handleLogin = () => {
     navigate('/login');
   };
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> 95ad50e445b23d590a7deb6951c25e40c200a56a
   const handleSignUp = () => {
     navigate('/signup');
   };
-<<<<<<< HEAD
- 
-  return (
-    <div className='home'>
-      <div className='left-h'>
-        <Header />
-        <div className='heading'>
-=======
+
 
   return (
     <div className="home">
       <div className="left-h">
         <Header />
         <div className="heading">
->>>>>>> 95ad50e445b23d590a7deb6951c25e40c200a56a
+
           <div>
             <span>Revolutionize </span>
             <span>Finance</span>
@@ -51,7 +31,7 @@ const Home = () => {
             <span>Embracing the Future</span>
           </div>
         </div>
-<<<<<<< HEAD
+
         <div className='para'>
           <span>Introducing the Pioneering Credit Discovery and Investment Platform, Redefining Possibilities in Finance</span>
         </div>
@@ -81,27 +61,4 @@ const Home = () => {
  
 export default Home;
  
-=======
-        <div className="para">
-          <span>Introducing the Pioneering Credit Discovery and Investment Platform, Redefining Possibilities in Finance</span>
-        </div>
-      </div>
-      <div className="right-h">
-        <div className="home-buttons">
-          <button className="btn" onClick={handleSignUp}>
-            Sign Up
-          </button>
-          <button className="btn btn-dark btn-block" onClick={handleLogin}>
-            Login
-          </button>
-        </div>
-      </div>
-      <div className="image-container">
-        <img className="image" src="./Images/Home_image.jpg" alt="Home Image" />
-      </div>
-    </div>
-  );
-};
-
-export default Home;
->>>>>>> 95ad50e445b23d590a7deb6951c25e40c200a56a
+        
