5a3ec09b7934d13c1e535eb3d0ea9df87fa83e05
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-11-30 22:14:00 +0530

    first commit

--- src/Components/Product/ProductMaster.js
+++ src/Components/Product/ProductMaster.js
@@ -4,29 +4,37 @@ import { InputText } from "primereact/inputtext";
 import { Calendar } from 'primereact/calendar';
 import { Button } from 'primereact/button';
 import "./ProductMaster.css";
+import { useNavigate } from 'react-router-dom';
 
 export default function BasicDemo() {
   const [value, setValue] = useState('');
   const [product, SetProductcode] = useState('');
   const [date, setDate] = useState(null);
   const [enddate, setEnddate] = useState(null);
+  const navigate = useNavigate();
+ 
+  
+ 
 
 
   return (
-      <div className="card flex justify-content-center">
-        <h3 className='product-heading'>Product Master</h3>
-        <label htmlFor="productname">Product Name</label>
+    
+      <div className="card flex justify-content-center" style={{width:"1000px"}}>
+        <h2 className='product-heading'>Product Master</h2>
+        <div className="justify-content">
+        <p style={{textAlign:"left"}}>Product Name</p>
           <InputText value={value} onChange={(e) => setValue(e.target.value)} />
-          <label htmlFor="productcode">Product Code</label>
+          <p style={{textAlign:"left"}}>Product Code</p>
           <InputText value={product} onChange={(e) => SetProductcode(e.target.product)} />
+          </div>
           <div className="justify-content">
-          <label htmlFor="productstartdate">Product Start Date</label>
-          <Calendar value={date} onChange={(e) => setDate(e.value)} showIcon />  </div>
-          <div>
-          <label htmlFor="productenddate">Product End Date</label>
-          <Calendar value={enddate} onChange={(e) => setEnddate(e.value)} showIcon />  </div>
-          <div className="justify-content-center">
-            <Button label="Submit" />
+          <p style={{textAlign:"left"}}>Product Start Date</p>
+          <Calendar value={date} onChange={(e) => setDate(e.value)} />         
+          <p style={{textAlign:"left"}}>Product End Date</p>
+          <Calendar value={enddate} onChange={(e) => setEnddate(e.value)} /> 
+           </div>
+          <div style={{marginLeft:"160px"}}>
+          <Button className="mt-4" label="Submit" onClick={()=>navigate('/')} />
         </div>
       </div>
       
